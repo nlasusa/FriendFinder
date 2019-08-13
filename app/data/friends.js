71f800d4628acfefc38425e9bd7d8c8cc3b1365c
Nicole LaSusa <nicolelasusa@gmail.com>
2019-08-13 18:29:22 -0400

    html and npm packages

---
+++ app/data/friends.js
@@ -0,0 +1,119 @@
+//array of objects
+var friends = [
+    {
+        name: "Kate Sullivan",
+        photo: "image.png",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Allie Hansen",
+        photo: "image.png",
+        scores: [
+            4,
+            1,
+            3,
+            3,
+            1,
+            5,
+            2,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Alex Frantzis",
+        photo: "image.png",
+        scores: [
+            2,
+            2,
+            3,
+            3,
+            4,
+            5,
+            4,
+            1,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Lori Bridda",
+        photo: "image.png",
+        scores: [
+            5,
+            2,
+            1,
+            1,
+            4,
+            1,
+            2,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Steph Favara",
+        photo: "image.png",
+        scores: [
+            1,
+            5,
+            4,
+            2,
+            1,
+            2,
+            5,
+            3,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Wendy Witt",
+        photo: "image.png",
+        scores: [
+            3,
+            3,
+            4,
+            1,
+            2,
+            2,
+            2,
+            3,
+            1,
+            4
+        ]
+    },
+    {
+        name: "Emily Christiansen",
+        photo: "image.png",
+        scores: [
+            5,
+            3,
+            4,
+            2,
+            1,
+            1,
+            4,
+            3,
+            1,
+            2
+        ]
+    }
+];
+
+//export module 
+module.export = friends;
+
---
+++ server.js
@@ -0,0 +1,23 @@
+// npm packages required
+var express = require("express"); 
+var bodyParser = require("body-parser");
+var path = require ("path");
+
+//port setup
+var app = express(); 
+var port = process.env.PORT || 3000; 
+
+//add body parser structure 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.text()); 
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+//apply routes
+app.use (express.static("app/public"));
+//api routes
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+//listening to port
+app.listen(port, () => console.log("Listening on port %s", port)); 
